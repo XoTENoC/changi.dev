104665180e101ad7124cb0c39a2743aaabd94450
Nathaniel Chang <changipro.nathaniel@gmail.com>
2022-05-27 18:48:47 +0930

    adding the style files, and header file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <app-header />
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from '@/components/AppHeader.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    AppHeader
   }
 }
 </script>
